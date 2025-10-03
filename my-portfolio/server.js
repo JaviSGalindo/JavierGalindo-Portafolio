1a55b8ee51390944b07521197475cf2e074ce3cd
JaviSGalindo <javier2006galindo@gmail.com>
2025-10-03 08:36:48 -0500

    get on ready for app service

---
+++ my-portfolio/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servir la carpeta pública
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Opcional para SPA / catch-all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
